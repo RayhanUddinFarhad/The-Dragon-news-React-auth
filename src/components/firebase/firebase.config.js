1e35e9f9a077bd2c189dffc55d89b693c25d3cc6
ITP <RayhanUddinFarhad11@gmail.com>
2023-04-30 23:29:59 +0600

    api key hidden

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwgHPUo506fI4SQrpXrn8DtYvKOfxj8x4",
-  authDomain: "the-dragon-news-7730b.firebaseapp.com",
-  projectId: "the-dragon-news-7730b",
-  storageBucket: "the-dragon-news-7730b.appspot.com",
-  messagingSenderId: "601060533056",
-  appId: "1:601060533056:web:422bb803869d68df5f10a5"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
